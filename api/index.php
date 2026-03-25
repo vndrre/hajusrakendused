551f91907724667700686ef213783d500c67aed8
vndrre <aleppik7@gmail.com>
2026-03-25 11:15:21 +0200

    deploy route bug

--- api/index.php
+++ api/index.php
@@ -2,5 +2,16 @@
 
 declare(strict_types=1);
 
-require __DIR__.'/../public/index.php';
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 
+$autoloadPath = __DIR__.'/../vendor/autoload.php';
+
+if (! is_file($autoloadPath)) {
+    http_response_code(500);
+    header('content-type: text/plain; charset=utf-8');
+    echo 'Missing vendor/autoload.php. Composer dependencies likely not installed in Vercel build.';
+    exit;
+}
+
+require __DIR__.'/../public/index.php';
